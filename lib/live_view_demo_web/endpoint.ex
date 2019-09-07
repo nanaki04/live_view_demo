a9eec0e05ad0f5d987ee953b6f2d0f13c4c1975c
zwetsloot robert jan <zwetsloot.robert.jan@techcross.co.jp>
2019-09-07 23:15:50 +0900

    Added setup for Heroku deploy

--- lib/live_view_demo_web/endpoint.ex
+++ lib/live_view_demo_web/endpoint.ex
@@ -2,10 +2,11 @@ defmodule LiveViewDemoWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :live_view_demo
 
   socket "/socket", LiveViewDemoWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
-  socket "/live", Phoenix.LiveView.Socket
+  socket "/live", Phoenix.LiveView.Socket,
+    websocket: [timeout: 45_000]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
